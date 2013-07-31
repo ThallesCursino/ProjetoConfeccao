5e44179ebd62c7aea444c413c655d83a1b9c66bc
thalles_cardin <thalles_cardin@10.1.48.255>
2013-07-31 15:41:28 -0300

    Fazendo a classe funcionario

---
+++ Revisao1/src/br/senai/sc/model/negocio/Funcionario.java
@@ -0,0 +1,53 @@
+
+package br.senai.sc.model.negocio;
+
+/**
+ * Classe que representa os funcionarios do software
+ * @version 1.0 31/07/13
+ * @author thalles_cardin
+ */
+public class Funcionario {
+    
+    private double salario;
+    private String dataAdmissao;
+    private String ctps;
+    private String cargo;
+
+    public Funcionario() {
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(String dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public String getCtps() {
+        return ctps;
+    }
+
+    public void setCtps(String ctps) {
+        this.ctps = ctps;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    
+    
+    
+}
